d1c92a29ccdf2c58a0551092046f5558d35e1286
Taiwo Olapade <semiteprofessor@gmail.com>
2024-05-15 14:57:38 +0100

    Commit

--- src/components/forms/AssProductForm.tsx
+++ src/components/forms/AssProductForm.tsx
@@ -31,49 +31,49 @@ import { productPayload, productSchema } from "@/lib/validators/product";
 import { FileUpload } from "./FileUpload";
 
 export function AddProductForm() {
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const params = useParams()
-  const router = useRouter()
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const params = useParams();
+  const router = useRouter();
 
   const form = useForm<productPayload>({
     resolver: zodResolver(productSchema),
     defaultValues: {
-      name: '',
-      description: '',
+      name: "",
+      description: "",
     },
-  })
+  });
 
   const onSubmit = async (values: productPayload) => {
     try {
-      setIsLoading(true)
+      setIsLoading(true);
       const { data }: { data: Product } = await axios.post(
         `/api/stores/${params.storeId}/products`,
-        values,
-      )
-      toast.success('Product is created.')
-      router.push(`/${data.storeId}/${data.slug}?productId=${data.id}`)
+        values
+      );
+      toast.success("Product is created.");
+      router.push(`/${data.storeId}/${data.slug}?productId=${data.id}`);
     } catch (error: any) {
-      toast.error(error.response.data)
+      toast.error(error.response.data);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
+  };
 
   return (
     <Form {...form}>
       <form
-        className='grid w-full max-w-xl gap-5'
+        className="grid w-full max-w-xl gap-5"
         onSubmit={form.handleSubmit(onSubmit)}
       >
         <FormField
           control={form.control}
-          name='name'
+          name="name"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Name</FormLabel>
               <FormControl>
                 <Input
-                  placeholder='Type product name here.'
+                  placeholder="Type product name here."
                   disabled={isLoading}
                   {...field}
                 />
@@ -84,13 +84,13 @@ export function AddProductForm() {
         />
         <FormField
           control={form.control}
-          name='description'
+          name="description"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Description</FormLabel>
               <FormControl>
                 <Textarea
-                  placeholder='Type product description here.'
+                  placeholder="Type product description here."
                   disabled={isLoading}
                   {...field}
                 />
@@ -99,12 +99,12 @@ export function AddProductForm() {
             </FormItem>
           )}
         />
-        <div className='flex flex-col items-start gap-6 sm:flex-row'>
+        <div className="flex flex-col items-start gap-6 sm:flex-row">
           <FormField
             control={form.control}
-            name='category'
+            name="category"
             render={({ field }) => (
-              <FormItem className='flex-1 w-full'>
+              <FormItem className="flex-1 w-full">
                 <FormLabel>Category</FormLabel>
                 <Select
                   value={field.value}
@@ -114,14 +114,14 @@ export function AddProductForm() {
                 >
                   <FormControl>
                     <SelectTrigger>
-                      <SelectValue placeholder='Select a category' />
+                      <SelectValue placeholder="Select a category" />
                     </SelectTrigger>
                   </FormControl>
                   <SelectContent>
-                    <SelectItem value='skateboards'>Skateboards</SelectItem>
-                    <SelectItem value='clothing'>Clothing</SelectItem>
-                    <SelectItem value='shoes'>Shoes</SelectItem>
-                    <SelectItem value='accessories'>Accessories</SelectItem>
+                    <SelectItem value="skateboards">Skateboards</SelectItem>
+                    <SelectItem value="clothing">Clothing</SelectItem>
+                    <SelectItem value="shoes">Shoes</SelectItem>
+                    <SelectItem value="accessories">Accessories</SelectItem>
                   </SelectContent>
                 </Select>
                 <FormMessage />
@@ -130,19 +130,19 @@ export function AddProductForm() {
           />
           <FormField
             control={form.control}
-            name='price'
+            name="price"
             render={({ field }) => (
-              <FormItem className='flex-1 w-full'>
+              <FormItem className="flex-1 w-full">
                 <FormLabel>Price</FormLabel>
                 <FormControl>
-                  <div className='relative'>
-                    <p className='absolute text-sm left-0 w-8 inset-y-0 grid place-items-center'>
+                  <div className="relative">
+                    <p className="absolute text-sm left-0 w-8 inset-y-0 grid place-items-center">
                       Rp
                     </p>
                     <Input
-                      type='number'
-                      className='pl-8'
-                      placeholder='0'
+                      type="number"
+                      className="pl-8"
+                      placeholder="0"
                       disabled={isLoading}
                       {...field}
                     />
@@ -153,3 +153,37 @@ export function AddProductForm() {
             )}
           />
         </div>
+        <FormField
+          control={form.control}
+          name="images"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Images</FormLabel>
+              <FormControl>
+                <FileUpload
+                  endpoint="imageUploader"
+                  value={field.value}
+                  onChange={(file) =>
+                    field.value
+                      ? field.onChange([...field.value, ...file])
+                      : field.onChange([...file])
+                  }
+                  onRemove={(url) =>
+                    field.onChange([
+                      ...field.value.filter((current) => current.url !== url),
+                    ])
+                  }
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button isLoading={isLoading}>
+          Add Product
+          <span className="sr-only">Add Product</span>
+        </Button>
+      </form>
+    </Form>
+  );
+}
