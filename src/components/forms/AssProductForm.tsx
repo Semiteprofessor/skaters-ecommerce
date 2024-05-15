5f3185aa799c4358000973dd1e2b0b81fffc069b
Taiwo Olapade <semiteprofessor@gmail.com>
2024-05-15 14:56:32 +0100

    Commit

---
+++ src/components/forms/AssProductForm.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Product } from "@prisma/client";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+
+import { Button } from "@/components/ui/Button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/Form";
+import { Input } from "@/components/ui/Input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/Select";
+import { Textarea } from "@/components/ui/Textarea";
+import { productPayload, productSchema } from "@/lib/validators/product";
+
+import { FileUpload } from "./FileUpload";
