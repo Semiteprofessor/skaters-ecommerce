dd1042bfe8675e23b4af02bc630ff7fab4a07cc2
Taiwo Olapade <semiteprofessor@gmail.com>
2024-05-15 14:58:42 +0100

    Commit

--- src/components/forms/AddStoreForm.tsx
+++ src/components/forms/AddStoreForm.tsx
@@ -20,25 +20,74 @@ import { Textarea } from '@/components/ui/Textarea'
 import { storePayload, storeSchema } from '@/lib/validators/store'
 
 export function AddStoreForm() {
-  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const form = useForm<storePayload>({
     resolver: zodResolver(storeSchema),
     defaultValues: {
-      name: '',
-      description: '',
+      name: "",
+      description: "",
     },
-  })
+  });
 
   const onSubmit = async (values: storePayload) => {
     try {
-      setIsLoading(true)
-      await axios.post('/api/stores', values)
-      toast.success('Store is created.')
-      window.location.assign('/dashboard/stores')
+      setIsLoading(true);
+      await axios.post("/api/stores", values);
+      toast.success("Store is created.");
+      window.location.assign("/dashboard/stores");
     } catch (error: any) {
-      toast.error(error.response.data)
+      toast.error(error.response.data);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        className="grid w-full max-w-xl gap-5"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Type store name here."
+                  disabled={isLoading}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Type store description here."
+                  disabled={isLoading}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button isLoading={isLoading}>
+          Add Store
+          <span className="sr-only">Add Store</span>
+        </Button>
+      </form>
+    </Form>
+  );
+}
