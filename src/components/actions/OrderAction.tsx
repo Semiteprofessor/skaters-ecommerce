9d0fe99a1bb8316ef437fe7e5f5c1984bf4db1a8
Taiwo Olapade <semiteprofessor@gmail.com>
2024-05-16 12:58:54 +0100

    Commit

--- src/components/actions/OrderAction.tsx
+++ src/components/actions/OrderAction.tsx
@@ -20,3 +20,47 @@ import {
 interface OrderActionProps {
   order: Order;
 }
+
+export const OrderAction: React.FC<OrderActionProps> = ({ order }) => {
+  const [loading, setLoading] = useState<boolean>(false)
+  const [open, setOpen] = useState<boolean>(false)
+
+  const router = useRouter()
+
+  const onDelete = async () => {
+    try {
+      setLoading(true)
+      await axios.delete(`/api/orders/${order.id}/cancel`)
+      router.push('/dashboard/orders?status=CANCELED')
+      toast.success('Order canceled.')
+    } catch (error) {
+      console.log(error)
+      if (error instanceof AxiosError) {
+        return toast.error(error.response?.data)
+      }
+      toast.error('Something went wrong')
+    } finally {
+      setLoading(false)
+      setOpen(false)
+    }
+  }
+
+  const onPay = () => {
+    if (order.token) {
+      // @ts-expect-error
+      window.snap.pay(order.token, {
+        onSuccess: () => {
+          toast.success('Payment success!')
+        },
+        onPending: () => {
+          toast('Waiting your payment..')
+        },
+        onError: () => {
+          toast.error('Payment failed, something went wrong')
+        },
+        onClose: () => {
+          toast.error('You have not completed the payment.')
+        },
+      })
+    }
+  }
