b62f12fc5050ca53bfe4c39a91301b6bdd76a860
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-07 18:20:42 +0100

    Commit

---
+++ src/app/(dashboard)/stores/[storeId]/components/cell-action.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import axios from 'axios'
+import { Edit, Eye, MoreHorizontal, Trash } from 'lucide-react'
+import { useParams, useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { toast } from 'react-hot-toast'
+
+import { AlertModal } from '@/components/modals/AlertModal'
+import { Button } from '@/components/ui/Button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/DropdownMenu'
+
+import { ProductColumn } from './columns'
+
+interface CellActionProps {
+  data: ProductColumn
+}
+
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const router = useRouter()
+  const params = useParams()
+
+  const [loading, setLoading] = useState(false)
+  const [open, setOpen] = useState(false)
+
+  const onView = () => {
+    router.push(`/${data.storeId}/${data.slug}?productId=${data.id}`)
+  }
