f743427030862f33d13ca7be96e3eb86e0c5e505
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-28 11:52:03 +0100

    Commit

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -15,18 +15,59 @@ interface FilterProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const Filter: React.FC<FilterProps> = ({ categories, className, ...props }) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
 
-  const selectedCategory = searchParams.get('category')
+  const selectedCategory = searchParams.get("category");
 
   const changeCategory = (categorySlug: string) => {
-    setIsOpen(false)
+    setIsOpen(false);
 
     if (categorySlug === selectedCategory) {
-      return window.location.assign('/products')
+      return window.location.assign("/products");
     }
 
-    window.location.assign(`/products?category=${categorySlug}`)
-  }
+    window.location.assign(`/products?category=${categorySlug}`);
+  };
+
+  return (
+    <div className={cn("flex", className)} {...props}>
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger asChild>
+          <Button className="flex items-center gap-x-2">
+            Filters
+            <Plus />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="right">
+          <div className="px-2 flex flex-col gap-4">
+            <h3 className="text-lg text-emerald-600 font-semibold">
+              Categories
+            </h3>
+            <Separator />
+            <div className="flex flex-wrap gap-2">
+              {categories.map((category) => (
+                <div
+                  key={category.slug}
+                  onClick={() => changeCategory(category.slug)}
+                  className="flex items-center"
+                >
+                  <Button
+                    variant={
+                      selectedCategory === category.slug ? "default" : "outline"
+                    }
+                  >
+                    {category.name}
+                  </Button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+};
+
+export default Filter;
